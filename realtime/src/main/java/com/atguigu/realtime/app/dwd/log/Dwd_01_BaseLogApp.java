607794dba0cc98131f126aebc080f84a19b2bfce
lzc <lzc@atguigu.cn>
2022-06-17 10:41:56 +0800

    update

--- realtime/src/main/java/com/atguigu/realtime/app/dwd/log/Dwd_01_BaseLogApp.java
+++ realtime/src/main/java/com/atguigu/realtime/app/dwd/log/Dwd_01_BaseLogApp.java
@@ -1,6 +1,7 @@
 package com.atguigu.realtime.app.dwd.log;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.realtime.app.BaseAppV1;
 import com.atguigu.realtime.common.Constant;
@@ -8,17 +9,30 @@ import com.atguigu.realtime.util.DateFormatUtil;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author lzc
  * @Date 2022/6/17 8:51
  */
 public class Dwd_01_BaseLogApp extends BaseAppV1 {
+    
+    private final String PAGE = "page";
+    private final String ERR = "err";
+    private final String DISPLAY = "display";
+    private final String ACTION = "action";
+    private final String START = "start";
+    
     public static void main(String[] args) {
         new Dwd_01_BaseLogApp().init(2001, 2, "Dwd_01_BaseLogApp", Constant.TOPIC_ODS_LOG);
     }
@@ -30,12 +44,102 @@ public class Dwd_01_BaseLogApp extends BaseAppV1 {
         SingleOutputStreamOperator<JSONObject> etledStream = etl(stream);
         // 2. 纠正新老客户 标记
         SingleOutputStreamOperator<JSONObject> validatedStream = validateNewOrOld(etledStream);
-        validatedStream.print();
-        
         // 3. 分流
-        
+        Map<String, DataStream<String>> streams = splitStream(validatedStream);
+        streams.get(PAGE).print(PAGE);
+        streams.get(ERR).print(ERR);
+        streams.get(DISPLAY).print(DISPLAY);
+        streams.get(ACTION).print(ACTION);
+        streams.get(START).print(START);
+    
         // 4. 不同的写入到不同的topic中
         
+    }
+    
+    private Map<String, DataStream<String>> splitStream(SingleOutputStreamOperator<JSONObject> stream) {
+        OutputTag<String> errTag = new OutputTag<String>("err") {};
+        OutputTag<String> displayTag = new OutputTag<String>("display") {};
+        OutputTag<String> actionTag = new OutputTag<String>("action") {};
+        OutputTag<String> pageTag = new OutputTag<String>("page") {};
+        
+        /*
+        主流:  启动
+        
+        侧输出流:  错误  页面   曝光   行动
+         */
+        SingleOutputStreamOperator<String> startStream = stream.process(new ProcessFunction<JSONObject, String>() {
+            @Override
+            public void processElement(JSONObject obj,
+                                       Context ctx,
+                                       Collector<String> out) throws Exception {
+                // 1. 如何页面都有可能会有err
+                if (obj.containsKey("err")) {
+                    ctx.output(errTag, obj.toJSONString());
+                    // 错误信息对其他的日志没有用处, 把错误信息删除
+                    obj.remove("err");
+                }
+                
+                // 2. 是否为 start
+                if (obj.containsKey("start")) {
+                    out.collect(obj.toString());
+                }else{
+                    // 其他日志
+    
+                    JSONObject common = obj.getJSONObject("common");
+                    JSONObject page = obj.getJSONObject("page");
+                    Long ts = obj.getLong("ts");
+    
+    
+                    // 1. 曝光表日志
+                    JSONArray displays = obj.getJSONArray("displays");
+                    if (displays != null) {
+                        for (int i = 0; i < displays.size(); i++) {
+                            JSONObject display = displays.getJSONObject(i);
+                            
+                            display.putAll(common);
+                            display.putAll(page);
+                            display.put("ts", ts);
+                            
+                            ctx.output(displayTag,display.toJSONString());
+                            
+                        }
+                        obj.remove("displays");
+                    }
+                    
+                    // 2. 活动日志
+                    JSONArray actions = obj.getJSONArray("actions");
+                    if (actions != null) {
+                        for (int i = 0; i < actions.size(); i++) {
+                            JSONObject action = actions.getJSONObject(i);
+                            action.putAll(common);
+                            action.putAll(page);
+                            action.put("ts", ts);
+                            
+                            ctx.output(actionTag,action.toJSONString());
+                        }
+                        obj.remove("actions");
+                    }
+                    // 3. 页面日志, 除了启动, 所有日志都有page
+                    if (obj.containsKey("page")) {
+                        ctx.output(pageTag,obj.toJSONString());
+                    }
+                }
+            }
+        });
+        
+        // 返回多个流
+        // 1. list集合  取的顺序要和存的顺序一致
+        // 2. 元组
+        // 3. map集合
+        Map<String, DataStream<String>> result = new HashMap<>();
+        result.put(PAGE, startStream.getSideOutput(pageTag));
+        result.put(ERR, startStream.getSideOutput(errTag));
+        result.put(DISPLAY, startStream.getSideOutput(displayTag));
+        result.put(ACTION, startStream.getSideOutput(actionTag));
+        result.put(START, startStream);
+        
+        return result;
+    
     }
     
     private SingleOutputStreamOperator<JSONObject> validateNewOrOld(SingleOutputStreamOperator<JSONObject> stream) {
