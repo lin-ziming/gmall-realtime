746954505690a1da557e1ea61d90b5a5fccbfaf3
lzc <lzc@atguigu.cn>
2022-06-17 14:39:48 +0800

    update

--- realtime/src/main/java/com/atguigu/realtime/util/AtguiguUtil.java
+++ realtime/src/main/java/com/atguigu/realtime/util/AtguiguUtil.java
@@ -1,6 +1,8 @@
 package com.atguigu.realtime.util;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author lzc
@@ -9,5 +11,13 @@ import java.text.SimpleDateFormat;
 public class AtguiguUtil {
     public static String toDate(Long ts) {
         return new SimpleDateFormat("yyyy-MM-dd").format(ts);
+    }
+    
+    public static <T>List<T> toList(Iterable<T> it) {
+        ArrayList<T> result = new ArrayList<>();
+        for (T t : it) {
+            result.add(t);
+        }
+        return result;
     }
 }
